b7c30fa0258d5ec325b956e8f91ead1d5a59a61d
Jim-Lii <343347795@tdsb.ca>
2026-02-12 16:09:22 +0000

    hjfskjbhgrf

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/*
+File Name: Strings and Output
+Author: Jim Li
+Date Created: Feb. 12, 2026
+Date Last Modified: Feb. 12, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +16,30 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+	System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+	System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+	System.out.println("* * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	String cat = "          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''";
+	System.out.println(cat);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	String word1 = "Fortnite ";
+	String word2 = "is awful";
+	System.out.println(word1 + word2);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	String word1 = "Don't ";
+	String word2 = "@ me";
+	String word3 = word1 + word2;
+	System.out.println(word3);
 	}
 
 
